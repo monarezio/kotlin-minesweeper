6349d8f9533d6c5220489917f2e82cd0575158b5
Samuel Kodytek <samuel@kodytek.cz>
2017-10-29 17:00:57 +0100

    Changed the resources

--- src/main/kotlin/net/monarezio/Run.kt
+++ src/main/kotlin/net/monarezio/Run.kt
@@ -8,6 +8,7 @@ import net.monarezio.domain.minesweeper.Game
 import net.monarezio.presentation.minesweeper.Minesweeper
 import net.monarezio.presentation.minesweeper.models.MinesweeperModel
 import tornadofx.*
+import java.io.File
 
 /**
  * Created by monarezio on 08/07/2017.
@@ -20,7 +21,11 @@ class Run: App(Minesweeper::class) {
     }
 
     init {
-        Font.loadFont(Run::class.java.getResource("/fonts/FontAwesome.otf").toExternalForm(), 10.0)
+
+
+        println(File("resources/fonts/FontAwesome.otf").absolutePath)
+
+        Font.loadFont("file:" + File("resources/fonts/FontAwesome.otf").path, 10.0)
         setInScope(MinesweeperModel(Game.createNewGame(10, (10 * 2).toInt())), DefaultScope)
     }
 
